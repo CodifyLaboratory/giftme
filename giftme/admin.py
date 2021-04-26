3f96e9c0fbb6610071bc9043640fd32b167183bc
nigoshaya <desm606@gmail.com>
2021-04-26 14:45:21 +0000

    added settings

--- giftme/admin.py
+++ giftme/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Holiday,  Wish, Booking
 
-admin.register(Holiday)
-admin.register(Wish)
-admin.register(Booking)
+admin.site.register(Holiday)
+admin.site.register(Wish)
+admin.site.register(Booking)
--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
 
     path('users/', UserViewSet.as_view({'get': 'list'})),
